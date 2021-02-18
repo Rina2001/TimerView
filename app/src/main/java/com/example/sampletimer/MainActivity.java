0e2e57dfd8f48506c4f31542792d8a23ebe62493
rina.chen <rina.chen@amkcambodia.com>
2021-02-18 11:55:34 +0700

    init

---
+++ app/src/main/java/com/example/sampletimer/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sampletimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.maxwellforest.blogtimer.TimerView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int TIMER_LENGTH = 60;
+
+    private com.maxwellforest.blogtimer.TimerView mTimerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTimerView = (TimerView) findViewById(R.id.timer);
+        mTimerView.start(TIMER_LENGTH);
+        final Button timerStartButton = (Button) findViewById(R.id.btn_timer_start);
+        timerStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTimerView.start(TIMER_LENGTH);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        mTimerView.stop();
+
+        super.onPause();
+    }
+
+}
---
+++ app/src/main/java/com/maxwellforest/blogtimer/TimerView.java
@@ -0,0 +1,168 @@
+package com.maxwellforest.blogtimer;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import com.example.sampletimer.R;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Countdown timer view.
+ *
+ * @author Mike Gouline
+ */
+public class TimerView extends View {
+
+    private static final int ARC_START_ANGLE = 270; // 12 o'clock
+
+    private static final float THICKNESS_SCALE = 0.03f;
+
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+
+    private RectF mCircleOuterBounds;
+    private RectF mCircleInnerBounds;
+
+    private Paint mCirclePaint;
+    private Paint textPainter;
+
+    private Paint fullBgPaint;
+
+    private float mCircleSweepAngle;
+    long second;
+
+
+
+    private ValueAnimator mTimerAnimator;
+
+    public TimerView(Context context) {
+        this(context, null);
+    }
+
+    public TimerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TimerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        int circleColor = Color.RED;
+
+        if (attrs != null) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TimerView);
+            if (ta != null) {
+                circleColor = ta.getColor(R.styleable.TimerView_circleColor, circleColor);
+                ta.recycle();
+            }
+        }
+
+        mCirclePaint = new Paint();
+        mCirclePaint.setAntiAlias(true);
+        mCirclePaint.setStrokeWidth(30);
+        mCirclePaint.setStyle(Paint.Style.STROKE);
+        mCirclePaint.setColor(circleColor);
+
+        textPainter = new Paint();
+        textPainter.setAntiAlias(true);
+        textPainter.setTextSize(60);
+        textPainter.setColor(circleColor);
+
+        fullBgPaint = new Paint();
+        fullBgPaint.setAntiAlias(true);
+        fullBgPaint.setStyle(Paint.Style.STROKE);
+        fullBgPaint.setStrokeWidth(30);
+        fullBgPaint.setColor(Color.GRAY);
+    }
+
+    @SuppressWarnings("SuspiciousNameCombination")
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec); // Trick to make the view square
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (w != oldw || h != oldh) {
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mBitmap.eraseColor(Color.TRANSPARENT);
+            mCanvas = new Canvas(mBitmap);
+        }
+
+        super.onSizeChanged(w, h, oldw, oldh);
+        updateBounds();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+          mCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+
+        canvas.drawArc(mCircleInnerBounds, ARC_START_ANGLE, 360, false, fullBgPaint);
+
+        mCanvas.drawArc(mCircleInnerBounds, ARC_START_ANGLE, mCircleSweepAngle, false, mCirclePaint);
+        mCanvas.drawText(""+second, mCircleInnerBounds.centerY(), mCircleInnerBounds.centerX(), textPainter);
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+
+    }
+
+    /**
+     *
+     * @param secs
+     * @description as Second
+     */
+    public void start(int secs) {
+        stop();
+
+        mTimerAnimator = ValueAnimator.ofFloat(0f, 1f);
+        mTimerAnimator.setDuration(TimeUnit.SECONDS.toMillis(secs));
+        mTimerAnimator.setInterpolator(new LinearInterpolator());
+        mTimerAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                second = animation.getCurrentPlayTime()/1000;
+                float animationVal = (float) animation.getAnimatedValue();
+                drawProgress(animationVal);
+            }
+        });
+        mTimerAnimator.start();
+    }
+
+    public void stop() {
+        if (mTimerAnimator != null && mTimerAnimator.isRunning()) {
+            mTimerAnimator.cancel();
+            mTimerAnimator = null;
+            drawProgress(0f);
+        }
+    }
+
+    private void drawProgress(float progress) {
+        mCircleSweepAngle = 360 * (-progress);
+
+        invalidate();
+    }
+
+    private void updateBounds() {
+        final float thickness = getWidth() * THICKNESS_SCALE;
+
+        mCircleOuterBounds = new RectF(0, 0, getWidth(), getHeight());
+        mCircleInnerBounds = new RectF(
+                mCircleOuterBounds.left + thickness,
+                mCircleOuterBounds.top + thickness,
+                mCircleOuterBounds.right - thickness,
+                mCircleOuterBounds.bottom - thickness);
+
+        invalidate();
+    }
+}
